d73ff9d091eef406791435f8ec22ef6f990ae4bf
Brent Condon <bcondon43@gmail.com>
2017-09-01 12:37:32 -0400

    created a screen class

--- RPG Map Editor/main.cpp
+++ RPG Map Editor/main.cpp
@@ -1,52 +1,24 @@
-#include <SFML\Graphics.hpp>
+#include "Screen.h"
 #include <iostream>
+#include <string>
+
 
 #define WIDTH 800
 #define HEIGHT 600
+#define TITLE "MAP EDITOR"
 
-void render(sf::RenderWindow* window) {
-	window->clear();
-	sf::RectangleShape tileHolder(sf::Vector2f(304, window->getSize().y - 20));
-	tileHolder.setFillColor(sf::Color(100, 250, 50));
-	tileHolder.setPosition(10, 10);
-	window->draw(tileHolder);
-	window->display();
-}
+
 
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(WIDTH , HEIGHT), "My window");
-	bool isFullscreen = false;
+	
+	Screen screen(WIDTH, HEIGHT, TITLE);
 	
     // run the program as long as the window is open
-    while (window.isOpen())
+    while (screen.isOpen())
     {
-        // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
-                window.close();
-			// Enter FullScreen
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::F11) && isFullscreen == false) {
-				window.create(sf::VideoMode().getDesktopMode(), "My Window", sf::Style::Fullscreen);
-				isFullscreen = true;
-			}
-			// Leave FullScreen
-			if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Escape) || sf::Keyboard::isKeyPressed(sf::Keyboard::F11) )&& isFullscreen == true ) {
-				window.create(sf::VideoMode(WIDTH, HEIGHT), "My window");
-				isFullscreen = false;
-			}
-			if (event.type == sf::Event::Resized)
-				window.setView(sf::View(sf::FloatRect(0.f,0.f,window.getSize().x,window.getSize().y)));
-			
-        }
-		
-		render(&window);
-
-		
-		
+		screen.input();
+		screen.render();
     }
 
     return 0;
