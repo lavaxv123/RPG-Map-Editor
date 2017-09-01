b497fd77c911860b9e5d56d678bda9a7fcc3ba82
David Church <David.J.Church@outlook.com>
2017-09-01 14:13:39 -0400

    Some stuff with the tile class.

--- RPG Map Editor/Tile.cpp
+++ RPG Map Editor/Tile.cpp
@@ -2,11 +2,25 @@
 
 
 
-Tile::Tile()
+Tile::Tile(std::string name, sf::Texture texture, unsigned int size)
 {
+	Tile::name = name;
+	Tile::size = size;
+
 }
 
+Tile::Tile(std::string name, sf::Color color, unsigned int size)
+{
+	Tile::name = name;
+	Tile::color = color;
+	Tile::size = size;
+}
 
 Tile::~Tile()
 {
+}
+
+void Tile::setFlag()
+{
+	
 }
